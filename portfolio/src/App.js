6460a550cddca409f94beeeb25c2a233da58862e
Brandon <battleb2014@gmail.com>
2020-08-31 16:16:31 -0400

    portfolio build

---
+++ portfolio/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='wrap'>
+        <h1 className='main'>Brandon May</h1><br />
+        <h2 className='main'>Front end <em className='red'>React </em>Developer</h2><br /><br />
+        <h2 className='projects'>Projects</h2><br />
+        <a href='https://html-tag-library.herokuapp.com/'><h3>Complete HTML tag library</h3></a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
