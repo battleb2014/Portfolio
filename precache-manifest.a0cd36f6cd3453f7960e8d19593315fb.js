f196893117530ac99acd492184da15687b98de7c
Brandon <battleb2014@gmail.com>
2020-09-07 23:00:39 -0400

    Updates

---
+++ precache-manifest.a0cd36f6cd3453f7960e8d19593315fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f8d4f16a375bd8cf8cbeb7446a6c149",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9bdf4ddf25133fc307f2",
+    "url": "/portfolio/static/css/main.2de1b527.chunk.css"
+  },
+  {
+    "revision": "97e63636bc8fb388b7b4",
+    "url": "/portfolio/static/js/2.ca42dc3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.ca42dc3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bdf4ddf25133fc307f2",
+    "url": "/portfolio/static/js/main.b56c7f01.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "269955ebcb68e3fc455a31c2c4c91399",
+    "url": "/portfolio/static/media/developer.269955eb.png"
+  }
+]);
