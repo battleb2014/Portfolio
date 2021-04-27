4180d1eb594d3fd21a586276eeb648f83a5940d1
Brandon <battleb2014@gmail.com>
2021-04-27 14:26:25 -0400

    App update

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import me from './me.jpg';
 import './App.css';
 
 function App() {
@@ -6,10 +7,12 @@ function App() {
     <div className='App'>
       <div className='wrap'>
         <h1 className='main'><span>&#x3C;</ span> Brandon May <span><span className='red'>&frasl;</span>&#x3E;</span></h1><br />
-        <h2 className='main'>Front end <em className='red'>React </em>Developer</h2><br /><br />
-        <h2 className='projects'>Projects</h2><br />
-        <a href='https://html-tag-library.herokuapp.com/'><h3>Complete HTML tag library</h3></a>
-        <a href='https://battleb2014.github.io/reactSocialCard/'><h3>React social card</h3></a>
+        <img src={me} alt='me' className='me' />
+        <h1 className='main'>Front End <em className='red'>React </em>Developer</h1><br /><br />
+        <h1 className='projects'>Projects</h1><br />
+        <a href='#'><h2>React Form Validation</h2></a>
+        <a href='https://html-tag-library.herokuapp.com/'><h2>Complete HTML tag library</h2></a>
+        <a href='https://battleb2014.github.io/reactSocialCard/'><h2>React social card</h2></a>
       </div>
     </div>
   );
